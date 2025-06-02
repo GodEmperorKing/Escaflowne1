f144aeb58e009c2ca0c6efd51925a8f31d1ae340
GodEmperorKing <youralltoeasy@gmail.com>
2025-06-01 17:30:29 -0700

    2-vpc.tf

---
+++ 2-vpc.tf
@@ -0,0 +1,30 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
+resource "google_compute_network" "main" {
+  name                            = "alien"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+
+resource "google_compute_network" "main2" {
+  name                            = "alien2"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+
+
+resource "google_compute_network" "main3" {
+  name                            = "alien3"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
